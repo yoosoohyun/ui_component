282deda8540a19243e96fd01d831aac27aeb4325
yoosoohyun <yoosu59@gmail.com>
2017-07-11 11:34:49 +0900

    1

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Administrator on 2017-07-03.
+ */
+
+// 변수선언 = require('모듈이름');
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var include = require('gulp-include');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
+
+/*
+
+gulp.task( task이름, 함수/익명함수 );
+
+ */
+
+gulp.task('hello1', function () {
+  return console.log('Hello World1!');
+});
+
+gulp.task('hello2', function () {
+  return console.log('Hello World2!');
+});
+
+gulp.task('hello3', function () {
+  return console.log('Hello World3!');
+});
+
+// pipe()는 모듈의 기능을 실행해주는 함수
+// 새로 고침
+gulp.task('livereload', function(){
+  gulp.src(['html/*', 'css/*', 'js/*', '*'])
+      .pipe( livereload() );
+});
+
+gulp.task('watch', function(){
+  livereload.listen();
+  gulp.watch('*', ['livereload']);
+  gulp.watch('html_src/**', ['include', 'livereload']);
+  gulp.watch('css_src/**', ['sass', 'livereload']);
+});
+
+// header, footer, 공통영역 분리
+gulp.task('include', function(){
+  gulp.src("html_src/*.html")
+      .pipe(include())
+      .on('error', console.log)
+      .pipe(gulp.dest("html/"));
+
+});
+
+// sass 실행
+gulp.task('sass', function(){
+  return gulp.src('css_src/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('css/'));
+});
+
+
+
+gulp.task('default', ['livereload', 'include', 'sass', 'watch']);
